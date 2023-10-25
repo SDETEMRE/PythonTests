260ec8719c3facf7cb466e7d3f63bcb87c9f077d
SDETEMRE <dincsoyemre@gmail.com>
2023-10-25 22:02:37 +0300

    day1

---
+++ day1/hello_world.py
@@ -0,0 +1,18 @@
+print("emre")
+print('emre') #this is print statement
+
+"""
+comment
+"""
+
+print('emre\ndincsoy')
+print('emre "di"')
+print("emre \"di\"")
+
+name = 'Emre'
+age = 43
+
+print('My name is {} and I am {} years old'.format(name,age))
+
+
+
