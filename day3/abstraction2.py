8419f22daf095f135141480dc969f604404605df
SDETEMRE <dincsoyemre@gmail.com>
2023-11-03 11:54:04 +0300

    day3

--- day3/abstraction2.py
+++ day3/abstraction2.py
@@ -1,3 +1,4 @@
+import math
 import numbers
 from abc import ABC, abstractmethod
 """
@@ -7,7 +8,11 @@ abstractmethod : annotations that can be given to abstract methods
 if you need all the modules of abc you may import like import abc, than class Shape(abc.ABC) and 
 method annotation should be @abc.abstractmethod
 """
+class Volume(ABC):
 
+    @abstractmethod
+    def volume(self):
+        pass
 class Shape(ABC):
     def __init__(self):
         self.name = type(self).__name__
@@ -20,8 +25,54 @@ class Shape(ABC):
         return f'{type(self).__name__}{self.__dict__}'
 
 
-class Square(Shape):
+class Square (Shape):
 
     def __init__(self, side):
         super().__init__()
         self.side = side
+
+    def area(self) -> numbers:
+        return self.side * self.side
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+
+    def area(self) -> numbers:
+        return math.pow(self.radius) * math.pi
+
+
+class Rectangle(Shape):
+
+    def __init__(self, width, length):
+        super().__init__()
+        self.width = width
+        self.length = length
+
+    def area(self) -> numbers:
+        return self.width * self.length
+
+
+class Cube(Shape, Volume):
+
+    def area(self) -> numbers:
+        pass
+
+    def volume(self):
+        pass
+
+
+class Cylinder(Shape, Volume):
+
+    def area(self) -> numbers:
+        pass
+
+    def volume(self):
+        pass
+
+square = Square(8)
+
+print(square.area())
+print(square)
