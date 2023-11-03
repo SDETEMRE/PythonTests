bb5a4bbdd8b09d77eee51afdc9a1216c6a946b1b
SDETEMRE <dincsoyemre@gmail.com>
2023-11-03 13:58:27 +0300

    day4

--- day4/final_keyword.py
+++ day4/final_keyword.py
@@ -15,3 +15,27 @@ class Employee:
 class Driver(Employee):
     def work(self):
         print('Driving')
+
+class Person:
+
+    def __init__(self, age: int):
+        self.__age = age
+
+    @property
+    def person_age(self):
+        return self.__age
+
+    @person_age.setter
+    def person_age(self, value):
+        raise RuntimeError(f' age is constant, can not be changed')
+
+
+person1 = Person(10)
+
+print(person1.person_age)
+
+# person1.person_age = 100
+
+print(person1.person_age)
+
+print(person1.__age)
---
+++ day4/polymorphism.py
@@ -0,0 +1,11 @@
+from day3.abstraction2 import Shape, Square, Rectangle
+
+shape1: Shape = Square(5)
+
+shape2: Shape = Rectangle(3, 4)
+
+def display_area(shape: Shape):
+    print(f'the {shape.name}\' area is {shape.area()} ')
+
+display_area(shape1)
+display_area(shape2)
